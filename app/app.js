c379fe02949853e5faab9db9ae4f6df5258768ec
git-username <git-email-address>
2014-08-06 22:48:38 -0400

    working on project

--- app/app.js
+++ app/app.js
@@ -1,3 +1,25 @@
-angular.module('calendarDemoApp', []);
+angular.module('calendarDemoApp', []).
+
+directive('calendarApp', function(){
+
+	restrict : 'E',
+
+		templateUrl : 'mega-video.html',
+
+		scope : true,
+
+		controller : function($scope, $element, $attrs){
+
+	};
+
+}).
+
+directive('calendarBody', function(){
+
+	
+
+	};
+
+});
 
 // your controller and directive code go here
